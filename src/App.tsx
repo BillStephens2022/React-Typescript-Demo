5a4016f8f5628f955be4eadca49440aab42953be
BillStephens2022 <two4onebill@yahoo.com>
2024-02-13 07:33:46 -0500

    added NewGoal component with a form for adding a new goal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header.tsx";
 import CourseGoalList from "./components/CourseGoalList.tsx";
 import goalsImg from "./assets/goals.jpg";
+import NewGoal from "./components/NewGoal.tsx";
 
 
 export type CourseGoal = {
@@ -33,7 +34,7 @@ const App = () => {
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <button onClick={handleAddGoal}>Add Goal</button>
+      <NewGoal />
       <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
     </main>
   );
