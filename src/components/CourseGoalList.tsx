6fc0c550ae6d4bb5d977de4cba515329ac0468a5
BillStephens2022 <two4onebill@yahoo.com>
2024-02-13 08:26:31 -0500

    added a infobox component that is conditionally rendered depending on number of goals

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,22 +1,45 @@
 import CourseGoal from "./CourseGoal";
-import { CourseGoal as CourseGoalType } from "../App.tsx"
+import InfoBox from "./InfoBox.tsx";
+import { CourseGoal as CourseGoalType } from "../App.tsx";
+import { ReactNode } from "react";
 
 type CourseGoalListProps = {
   goals: CourseGoalType[];
   onDeleteGoal: (id: number) => void;
 };
 
-const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps ) => {
+const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+    {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
